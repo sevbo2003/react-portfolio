8db9adba61cefb60d36e64ba9d2d0731f42ddf30
sevbo2003 <sevbofx@gmail.com>
2022-04-21 00:42:44 +0500

    debug=false

--- config/settings.py
+++ config/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['139.59.253.7', '0.0.0.0']
 
