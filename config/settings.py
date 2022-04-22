437ff026eb20bb2cb561181db4fb4ea0028ce7b9
sevbo2003 <sevbofx@gmail.com>
2022-04-22 09:22:53 +0500

    Host added

--- config/settings.py
+++ config/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['139.59.253.7', '0.0.0.0']
+ALLOWED_HOSTS = ['139.59.253.7', '0.0.0.0', 'abdusamad.uz']
 
 # Application definition
 
