d6f79721c83dc5e8665bb65821dfb05b6c322d10
sevbo2003 <sevbofx@gmail.com>
2022-05-07 13:45:08 +0500

    User added

--- config/settings.py
+++ config/settings.py
@@ -92,7 +92,7 @@ else:
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
             'NAME': env('NAME'),
-            'USER': env("USER"),
+            'USER': "abdusamad",
             'PASSWORD': env("PASSWORD"),
             'HOST': '127.0.0.1',
             'PORT': '5432',
